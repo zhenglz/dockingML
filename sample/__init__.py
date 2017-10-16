3fbc1a854e131f29381c1b0e2943adb3d40e7a01
astrozheng <astrozheng@gmail.com>
2017-10-16 11:47:05 +0800

    Merged from upstream

--- sample/__init__.py
+++ sample/__init__.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
 from . import autoMD, extractFeatures
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+DEFINITIONS_ROOT = os.path.join(PROJECT_ROOT, 'sample', 'lib')
+
+if __name__ == "__main__" :
+    pass
+
 
