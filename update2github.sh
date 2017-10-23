19ef7cbdbf5cc1c8a8cde5afc6d20135c99857e3
astrozheng <astrozheng@gmail.com>
2017-10-23 19:39:13 +0800

    commits from hbutterfly

---
+++ update2github.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# reference
+# https://help.github.com/articles/adding-a-file-to-a-repository-using-the-command-line/
+
+git add --all
+git commit -m "commits from hbutterfly"
+git push origin master
+
