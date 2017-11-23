1a345301c35d9b699d1f1e0e777dacc717791532
astrozheng <astrozheng@gmail.com>
2017-11-23 16:23:25 +0800

    commits from hbutterfly

--- MDAnalysis/__init__.py
+++ MDAnalysis/__init__.py
@@ -2,5 +2,9 @@
 
 import os
 
+from .workingflow import *
+from .matrix import *
+from .network import *
+
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 DEFINITIONS_ROOT = os.path.join(PROJECT_ROOT, 'sample', 'lib')
