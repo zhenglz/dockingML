d901024872efaa5478a49ad3b4ec1de16e709b6e
zhenglz <astrozheng@gmail.com>
2018-01-25 15:18:25 +0800

    commits from mars

--- mdanaly/__init__.py
+++ mdanaly/__init__.py
@@ -7,6 +7,7 @@ from .matrix import *
 from .network import *
 from .cmap import *
 from .dynamics import *
+from .pmf import *
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 DEFINITIONS_ROOT = os.path.join(PROJECT_ROOT, 'sample', 'lib')
