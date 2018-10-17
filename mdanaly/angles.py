16ee9336a3813c4880913eb6ea6a343a19d1d860
zhenglz <astrozheng@gmail.com>
2018-10-17 23:28:34 +0800

    add sine value for angles module

--- mdanaly/angles.py
+++ mdanaly/angles.py
@@ -251,6 +251,7 @@ def gmxangle(args):
             print("Write angles to output file: ", args.o)
 
         # write angles to an output file
+        print("Writing angles to output file %s ......" % args.o)
         write_angles(angles, args.o, cosine=args.cos, dt=args.dt)
 
     else:
