4fc48373a6f06d08c7cd77ee97fadad1a607c20e
zhenglz <astrozheng@gmail.com>
2018-12-31 15:52:31 +0800

    update docstring in gmx index

--- update2github.sh
+++ update2github.sh
@@ -4,6 +4,6 @@
 # https://help.github.com/articles/adding-a-file-to-a-repository-using-the-command-line/
 
 git add --all
-git commit -m "update cmap"
+git commit -m "update docstring in gmx index "
 git push origin master
 
